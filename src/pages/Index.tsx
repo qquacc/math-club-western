091ec250742bb3ace95df9216a3c3d0f134f697f
Edward Xue <pearofruit@gmail.com>
2025-08-27 15:48:34 -0600

    deleted useless files

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,51 +7,51 @@ import CallToAction from "@/components/CallToAction";
 import Footer from "@/components/Footer";
 
 const Index = () => {
-	return (
-		<div className="relative min-h-screen">
-			<div className="relative">
-				<Header />
+  return (
+    <div className="relative min-h-screen">
+      <div className="relative">
+        <Header />
 
-				{/* Hero Section */}
-				<div className="bg-gradient-to-br from-gray-50 to-blue-50">
-					<Hero />
-				</div>
+        {/* Hero Section */}
+        <div className="bg-gradient-to-br from-gray-50 to-blue-50">
+          <Hero />
+        </div>
 
-				{/* Gradient Divider */}
-				<div className="h-24 bg-gradient-to-b from-white to-white" />
+        {/* Gradient Divider */}
+        <div className="h-24 bg-gradient-to-b from-white to-white" />
 
-				{/* Recent Events Section */}
-				<div className="bg-white">
-					<RecentEvents />
-				</div>
+        {/* Recent Events Section */}
+        <div className="bg-white">
+          <RecentEvents />
+        </div>
 
-				{/* Gradient Divider */}
-				<div className="h-24 bg-gradient-to-b from-white to-gray-50" />
+        {/* Gradient Divider */}
+        <div className="h-24 bg-gradient-to-b from-white to-gray-50" />
 
-				{/* Programs Section */}
+        {/* Programs Section 
 				<div className="bg-gray-50">
 					<ProgramsSection />
-				</div>
+				</div> */}
 
-				{/* Gradient Divider */}
-				<div className="h-24 bg-gradient-to-b from-gray-50 to-blue-50" />
+        {/* Gradient Divider */}
+        <div className="h-24 bg-gradient-to-b from-gray-50 to-blue-50" />
 
-				{/* Features Section */}
+        {/* Features Section 
 				<div className="bg-blue-50">
 					<Features />
-				</div>
+				</div> */}
 
-				{/* Gradient Divider */}
-				<div className="to-purple-150 h-12 bg-gradient-to-b from-blue-50" />
+        {/* Gradient Divider */}
+        <div className="to-purple-150 h-12 bg-gradient-to-b from-blue-50" />
 
-				{/* Call to Action */}
-				<CallToAction />
+        {/* Call to Action */}
+        <CallToAction />
 
-				{/* Footer */}
-				<Footer />
-			</div>
-		</div>
-	);
+        {/* Footer */}
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Index;
