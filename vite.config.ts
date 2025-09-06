a5b4b7e9a8ff0dc71026faacc2d72d514b3b8ec1
qua.cc <sblueduck@gmail.com>
2025-09-06 16:53:43 -0600

    Bugfixes for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const base = process.env.deploy ? "/" : "/math-club-western/";
-
 export default defineConfig(({ mode }) => ({
-	base: base,
-
+	base: "/",
 	server: {
 		host: "::",
 		port: 8080,
